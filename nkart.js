2da37862771c72344d52d29fc069fc6c3e7f210a
Uxa <kirill.yu99@gmail.com>
2021-08-09 10:27:54 +1000

    nav fix

--- nkart.js
+++ nkart.js
@@ -42,3 +42,8 @@ let menuLinks = Array.from(navLinks.querySelectorAll('a'));
 menuLinks.forEach(link => {
     link.onclick = () => mobileMenuShowDown();
 });
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
